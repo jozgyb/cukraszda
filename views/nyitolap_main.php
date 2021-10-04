e4cf6be01f34a99b0e838f75b2c6aca0b3323984
Bence <jzsab1@gmail.com>
2021-10-04 20:27:36 +0200

    main page initial concept

--- views/nyitolap_main.php
+++ views/nyitolap_main.php
@@ -1,5 +1,26 @@
-<h2>
-    <br>Üdvözöljük az új rendszerünkben!<br>
-    <br>Jó munkát kívánunk a rendszerünkkel!<br>
-    <br>Kezdhet ...<br>
-</h2>
+<div class="row">
+    <div class="col-xs-12 col-sm-6 col-md-8 col-centered">
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="views/img/nyitolap.jpg" alt="Kínálat">
+                    <div id="carousel-caption d-none d-md-block">
+                        <h5>
+                            Üdvözöljük a weboldalunkon
+                        </h5>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<p>
+    A cégünk 10 éve tevékenykedik a piacon. Fő árúcikkeink a kézműves sütémények és fagylaltok. Az elmúlt évek során számos kitüntetést és díjat nyertünk.
+    Magas szintű szakértelmünknek és kitartásunknak köszönhetően.
+</p>
+<p>
+    Az alábbiakban megtekintheti termékeinket.
+</p>
--- views/page_main.php
+++ views/page_main.php
@@ -2,24 +2,23 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>MVC - PHP</title>
+        <title>Cukrászda</title>
         <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT?>css/main_style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
         <?php if($viewData['style']) echo '<link rel="stylesheet" type="text/css" href="'.$viewData['style'].'">'; ?>
     </head>
     <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
         <header>
             <div id="user"><em><?= $_SESSION['userlastname']." ".$_SESSION['userfirstname'] ?></em></div>
-            <h1 class="header">Web-programozás II - MVC alkalmazás</h1>
+            <h1 class="header">Cukrászda</h1>
         </header>
         <nav>
             <?php echo Menu::getMenu($viewData['selectedItems']); ?>
         </nav>
-        <aside>
-                <p>Phasellus wisi nulla...</p>
-        </aside>
-        <section>
+        <div class="container-fluid">
             <?php if($viewData['render']) include($viewData['render']); ?>
-        </section>
+        </div>
         <footer>&copy; NJE - GAMF - Informatika Tanszék <?= date("Y") ?></footer>
     </body>
 </html>
