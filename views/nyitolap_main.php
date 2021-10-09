7699c273940c7dd3122eebff22d6525b89f6cc8d
Patri247 <75403472+Patri247@users.noreply.github.com>
2021-10-09 19:44:49 +0200

    Főoldal fejlesztése

--- views/nyitolap_main.php
+++ views/nyitolap_main.php
@@ -1,26 +1,39 @@
-<div class="row">
-    <div class="col-xs-12 col-sm-6 col-md-8 col-centered">
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="views/img/nyitolap.jpg" alt="Kínálat">
-                    <div id="carousel-caption d-none d-md-block">
-                        <h5>
-                            Üdvözöljük a weboldalunkon
-                        </h5>
-                        <p></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+<div class="kulso">
+<img class="d-block w-100" src="views/img/nyitolap.jpg" alt="Kínálat">
+<div class="also">
+<p>Üdvözöljük a weboldalunkon</p>
 </div>
-
-
+</div>
+<div class="bekezdes">
 <p>
     A cégünk 10 éve tevékenykedik a piacon. Fő árúcikkeink a kézműves sütémények és fagylaltok. Az elmúlt évek során számos kitüntetést és díjat nyertünk.
     Magas szintű szakértelmünknek és kitartásunknak köszönhetően.
 </p>
 <p>
-    Az alábbiakban megtekintheti termékeinket.
+    Az alábbiakban megtekintheti termékeinket:
 </p>
+</div>
+<div class="fagyi">
+<div class="kep">
+<img class="d-block w-100 fel" src="views/img/fagyi.jpg" alt="Kínálat">
+</div>
+<div class="szoveg">
+A kézműves fagylataink.
+</div>
+</div>
+
+
+<div class="süti">
+
+<div class="szoveg">
+A kézműves süteményeink.
+</div>
+<div class="kep">
+<img class="d-block w-100 fel" src="views/img/süti.jpg" alt="Kínálat">
+</div>
+
+</div>
+
+<div class="egyéb">
+</div>
--- views/page_main.php
+++ views/page_main.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Cukrászda</title>
+        <title>J & G Cukrászda</title>
         <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT?>css/main_style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
         <?php if($viewData['style']) echo '<link rel="stylesheet" type="text/css" href="'.$viewData['style'].'">'; ?>
@@ -11,7 +11,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
         <header>
             <div id="user"><em><?= $_SESSION['userlastname']." ".$_SESSION['userfirstname'] ?></em></div>
-            <h1 class="header">Cukrászda</h1>
+            <h1 class="header">J & G Cukrászda</h1>
         </header>
         <nav>
             <?php echo Menu::getMenu($viewData['selectedItems']); ?>
