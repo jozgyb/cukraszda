e41f19a087dfa95e0bdbb4ac7412b1ebe59824a9
Bence <jzsab1@gmail.com>
2021-11-07 16:50:38 +0100

    nyitolap css fix

--- views/page_main.php
+++ views/page_main.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>J & G Cukrászda</title>
-        <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT?>css/main_style.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-        <?php if($viewData['style']) echo '<link rel="stylesheet" type="text/css" href="'.$viewData['style'].'">'; ?>
-    </head>
-    <body>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-        <header>
-		
-            
-			<h1 class="header">J & G Cukrászda</h1>
-		
-        </header>
-        <nav>
-	<div id="user"><em><?=  $_SESSION['userlastname']." ".$_SESSION['userfirstname']." ".$_SESSION['bejelentkezes'] ?> </em></div>
-            <?php echo Menu::getMenu($viewData['selectedItems']); ?>
-				
-        </nav>
-        <div class="container-fluid">
-            <?php if($viewData['render']) include($viewData['render']); ?>
-        </div>
-        <footer>&copy; NJE - GAMF - Informatika Tanszék <?= date("Y") ?></footer>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <title>J & G Cukrászda</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT ?>css/main_style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <?php if ($viewData['style']) echo '<link rel="stylesheet" type="text/css" href="' . $viewData['style'] . '">'; ?>
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <header>
+        <h1 class="header">J & G Cukrászda</h1>
+    </header>
+    <nav class="navbar navbar-light">
+        <?php if (isset($_SESSION['bejelentkezes'])) { ?>
+            <div id="user"><em><?= $_SESSION['userlastname'] . " " . $_SESSION['userfirstname'] . " " . $_SESSION['bejelentkezes'] ?> </em></div>
+        <?php } ?>
+        <?php echo Menu::getMenu($viewData['selectedItems']); ?>
+    </nav>
+    <div class="container-fluid">
+        <?php if ($viewData['render']) include($viewData['render']); ?>
+    </div>
+    <footer>&copy; NJE - GAMF - Informatika Tanszék <?= date("Y") ?></footer>
+</body>
+
 </html>
