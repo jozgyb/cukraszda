ea512430f731d68d647147cee4d1e3e4f8704262
Patri247 <75403472+Patri247@users.noreply.github.com>
2021-11-08 21:24:21 +0100

    nyitólap

--- views/nyitolap_main.php
+++ views/nyitolap_main.php
@@ -7,34 +7,43 @@
 </div>
 <div class="bekezdes">
 <p>
-    A cégünk 10 éve tevékenykedik a piacon. Fő árúcikkeink a kézműves sütémények és fagylaltok. Az elmúlt évek során számos kitüntetést és díjat nyertünk.
-    Magas szintű szakértelmünknek és kitartásunknak köszönhetően.
+  <h3><i>Bemutatkozás </i></h3>
+
+Munkatársainkkal nap mint nap azért dolgozunk, hogy minőségi, friss és finom termékek közül válogathass nálunk, bármikor is térsz be a cukrászdába. Ugyanis ezeknek a finomságoknak az élet minden fontos pillanatánál helye van. Amikor egy kicsit megállhatsz a hétköznapok rohanásában és azzal foglalkozhatsz, ami igazán kikapcsol. Akkor, mikor végre egy asztalnál ül a család minden tagja, akikkel olyan ritkán tölthetsz időt. Olyankor, amikor találkozol a rég nem látott barátaiddal, akikkel végre van lehetőséged egy kicsit beszélgetni. Olyan alkalmakkor, amikor meglepnéd a szeretteidet egy kis édességgel. Mi pedig szeretnénk mindezt megédesíteni a számotokra. Hiszen a tortáink és süteményeink ott vannak a születésnapokon, az évfordulókon, a ballagásokon, az esküvőkön, valamint a szimpla hétköznap délutánokon egyaránt.
 </p>
-<p>
-    Az alábbiakban megtekintheti termékeinket:
-</p>
+
+</div>
+<div class="bekezdes">
+  <h3><i>Miből készítjük a süteményeket és a tortákat?</i></h3>
+
+Minden termék, amit a cukrászdánk pultjában találsz, a saját kezünk munkája, saját üzemünkben készítjük ezeket munkatársainkkal együtt. Fontosnak tartjuk, hogy minden esetben kiváló minőségű alapanyagokat használjunk mind a hagyományos, mind pedig az általunk kitalált receptek esetében. Valójában csak olyan süteményeket gyártunk, amelyeket mi magunk is nagyon szeretünk. Ezért van az is, hogy valódi csokoládéból és igazi étcsokival dolgozunk, de imádjuk a hamisítatlan zamatos gyümölcsöket is, valamint az üzemben használt vajat és tejszínt is. Ettől olyan különleges.
 </div>
 <div class="fagyi">
 <div class="kep">
-<img class="d-block w-100 fel" src="views/img/fagyi.jpg" alt="Kínálat">
-</div>
-<div class="szoveg">
-A kézműves fagylataink.
-</div>
-</div>
-
-
-<div class="süti">
-
-<div class="szoveg">
-A kézműves süteményeink.
+<img class="d-block w-100 fel" src="views/img/kep1.jpg" >
 </div>
 <div class="kep">
-<img class="d-block w-100 fel" src="views/img/süti.jpg" alt="Kínálat">
+<img class="d-block w-100 fel" src="views/img/kep2.jpg" >
+</div>
+<div class="kep">
+<img class="d-block w-100 fel" src="views/img/kep3.jpg">
+</div>
+</div>
+<div class="bekezdes">
+  <h3><i>Hogy zajlik az online rendelés?</i></h3>
+
+Szerettük volna, ha kényelmesen válogathatsz a termékeink közül akár otthonról, a fotelben ülve is. Azt is tudjuk, milyen nehéz választani a számtalan sütemény és torta közül úgy, hogy közben a sorban állsz. Ezért most lehetőséget adunk neked minderre! Nézz szét a termékeink között, válaszd ki, miből mekkora mennyiségre van szükséged, majd válaszd ki a naptárban a számodra megfelelő időpontot. Fontos tudnod, hogy mivel minden frissen készül nálunk, legalább 48 órával előbb kell leadnod a rendelésedet, az így leadott rendelések személyes átvételére pedig nyíregyházi cukrászdánkban van lehetőséged. Ezt követően csupán annyi a feladatod, hogy ellátogass a cukrászdába az általad kiválasztott napon, ahol már vár majd a csomagod
+</div>
+<div style="margin-bottom: 0px;" class="fagyi">
+<div class="kep">
+<img class="d-block w-100 fel" src="views/img/kep4.jpg" >
 </div>
 
+
+</div>
+</div>
 </div>
 
-<div class="egyéb">
-</div>
+
+
 <?php 	Menu::setMenu(); ?>
